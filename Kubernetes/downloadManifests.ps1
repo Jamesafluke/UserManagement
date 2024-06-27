365f602795d6a53b92cf4b01793b2970b7edbda2
James Fluckiger <jamesafluke@gmail.com>
2024-06-26 21:51:12 -0600

    Add limits

--- Kubernetes/downloadManifests.ps1
+++
@@ -1,5 +0,0 @@
-$url = 'https://raw.githubusercontent.com/Jamesafluke/UserManagement/main/Kubernetes/deployment.yaml'
-$url = 'https://raw.githubusercontent.com/Jamesafluke/UserManagement/main/Kubernetes/service.yaml'
-$outputPath = '$(Build.ArtifactStagingDirectory)/deployment.yaml'
-Invoke-WebRequest -Uri $url -OutFile $outputPath
-Write-Host "Downloaded manifest to $outputPath"
